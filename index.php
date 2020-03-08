6f0d5b1bf37f172806a55d646a5b0bacdf6b661a
mihai <mihai.tuhari@gmail.com>
2020-03-08 17:44:05 +0200

    all: style

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8"/>
     <title>snow</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta content="en" name="language"/>
     <meta content="snow" name="title"/>
     <meta content="#gooutandexplore" name="description" property="og:description"/>
