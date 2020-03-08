5d17d89de1e607938fa42a4903f75650910c31a4
Stefan Radu <radu.stefan@amplicare.com>
2020-03-08 21:53:24 +0200

    Update webcams/transalpina.php
    Co-Authored-By: mihaituhari <mihai.tuhari@gmail.com>

--- webcams/transalpina.php
+++ webcams/transalpina.php
@@ -8,6 +8,6 @@
 <div class="webcam">
     <span>Sosire Telescaun</span>
     <div>
-        <iframe src="http://82.79.247.31:4000/cgi-bin/faststream.jpg?stream=full&fps=12" frameborder="0"></iframe>
+        <img src="http://82.79.247.31:4000/cgi-bin/faststream.jpg?stream=full&fps=12"/>
     </div>
 </div>
