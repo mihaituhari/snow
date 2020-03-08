3badc481d79c4304c52f08910131a367ddffd261
mihai <mihai.tuhari@gmail.com>
2020-03-08 22:27:41 +0200

    scripts: extend image proxy handler for Transalpina

--- scripts/image.php
+++ scripts/image.php
@@ -1,16 +1,23 @@
 <?php declare(strict_types=1);
 
-require_once __DIR__ . '/libs.php';
+require_once __DIR__ . '/init.php';
 
 /** @var string|null $imageUrl */
 $imageUrl = $_GET['url'] ?? null;
 
-if (!$imageUrl) {
+/** @var string|null $refererKey */
+$refererKey = $_GET['referer'] ?? null;
+
+if (!$imageUrl || !$refererKey) {
     http_response_code(404);
 }
 
-/** @var string $referer */
-$referer = 'https://www.freecam.ro/partii-de-schi/partia-de-schi-poiana-brasov';
+/** @var string|null $referer */
+$referer = $referers[$refererKey] ?? null;
+
+if (!$referer) {
+    http_response_code(412);
+}
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $imageUrl);
